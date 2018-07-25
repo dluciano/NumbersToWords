53f75ee71d176c990193757dcd592ed3c4c801d8
Dawlin Pena Luciano <dawmusic19@gmail.com>
2018-07-25 01:38:18 +0100

    Ones working

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -9,6 +9,15 @@ namespace Tests
     {
         [Theory]
         [InlineData(0, "zero")]
+        [InlineData(1, "one")]
+        [InlineData(2, "two")]
+        [InlineData(3, "three")]
+        [InlineData(4, "four")]
+        [InlineData(5, "five")]
+        [InlineData(6, "six")]
+        [InlineData(7, "seven")]
+        [InlineData(8, "eight")]
+        [InlineData(9, "nine")]
         public void OnesTests(int e, string name) =>
             e.ToWords().ShouldBe(name);
     }
