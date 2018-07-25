43f5acd24d71c000628ed385429416cecf58b1c5
Dawlin Pena Luciano <dawmusic19@gmail.com>
2018-07-25 02:20:39 +0100

    Twenties working

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -3,9 +3,9 @@ using System;
 using ToWords;
 using Xunit;
 
-namespace Tests
+namespace UnitTests
 {
-    public class Tests
+    public class NumberToWordsTests
     {
         [Theory]
         [InlineData(0, "zero")]
@@ -19,6 +19,20 @@ namespace Tests
         [InlineData(8, "eight")]
         [InlineData(9, "nine")]
         public void OnesTests(int e, string name) =>
+            e.ToWords().ShouldBe(name);
+
+        [Theory]
+        [InlineData(20, "twenty")]
+        [InlineData(21, "twenty-one")]
+        [InlineData(22, "twenty-two")]
+        [InlineData(23, "twenty-three")]
+        [InlineData(24, "twenty-four")]
+        [InlineData(25, "twenty-five")]
+        [InlineData(26, "twenty-six")]
+        [InlineData(27, "twenty-seven")]
+        [InlineData(28, "twenty-eight")]
+        [InlineData(29, "twenty-nine")]
+        public void Twenties(int e, string name) =>
             e.ToWords().ShouldBe(name);
     }
 }
--- ToWords/Extensions.cs
+++ ToWords/Extensions.cs
@@ -4,32 +4,76 @@ namespace ToWords
 {
     public static class Extensions
     {
-        public static string ToWords(this int val) {
-            switch (val)
-            {
-                case 0:
-                    return "zero";
-                case 1:
-                    return "one";
-                case 2:
-                    return "two";
-                case 3:
-                    return "three";
-                case 4:
-                    return "four";
-                case 5:
-                    return "five";
-                case 6:
-                    return "six";
-                case 7:
-                    return "seven";
-                case 8:
-                    return "eight";
-                case 9:
-                    return "nine";
 
-                default:
-                    return "Not defined";
+        public static string ToWords(this int val)
+        {
+            var ones = val % 10;
+            int tens = val / 10;
+
+            return Tens(tens, ones);
+
+            string Ones(int n)
+            {
+                switch (n)
+                {
+                    case 0:
+                        return "zero";
+                    case 1:
+                        return "one";
+                    case 2:
+                        return "two";
+                    case 3:
+                        return "three";
+                    case 4:
+                        return "four";
+                    case 5:
+                        return "five";
+                    case 6:
+                        return "six";
+                    case 7:
+                        return "seven";
+                    case 8:
+                        return "eight";
+                    case 9:
+                        return "nine";
+                    default:
+                        return "Not defined";
+                }
+            }
+
+            string Tens(int t, int o, char separator = '-')
+            {
+                var r = "";
+                switch (t)
+                {
+                    case 0:
+                        r = string.Empty;
+                        break;
+                    //case 1:
+                    //    return "one";
+                    case 2:
+                        r = "twenty";
+                        break;
+                    //case 3:
+                    //    return "three";
+                    //case 4:
+                    //    return "four";
+                    //case 5:
+                    //    return "five";
+                    //case 6:
+                    //    return "six";
+                    //case 7:
+                    //    return "seven";
+                    //case 8:
+                    //    return "eight";
+                    //case 9:
+                    //    return "nine";
+                    default:
+                        throw new NotImplementedException();
+                }
+                return r +
+                    (o == 0 || t == 0 ? String.Empty : separator.ToString()) +
+                    (t == 0 ? Ones(o) : o == 0 ? String.Empty : Ones(o));
             }
         }
     }
